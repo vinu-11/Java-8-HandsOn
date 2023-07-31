32178c851affca0bc08711cf9ea16d59c6cee2e8
rubenpa1998 <rubenpa1998@gmail.com>
2023-07-31 17:32:36 +0530

    Created - Added programs for functional folder

---
+++ functional/FunctionDemo.java
@@ -0,0 +1,23 @@
+package functional;
+
+import java.util.function.Function;
+
+class FunctionalImpl implements Function<String, Integer> {
+
+    @Override
+    public Integer apply(String t) {
+        return t.length();
+    }
+
+}
+
+public class FunctionDemo {
+    public static void main(String[] args) {
+        // Function<String, Integer> function = new FunctionalImpl();
+        // System.out.println(function.apply("Vinaykumar"));
+
+        Function<String, Integer> function = (String t) -> (t.length());
+        System.out.println(function.apply("vinaykumar"));
+
+    }
+}
