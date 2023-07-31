cff9a4a15381225ccdb08709cb10ae942c089925
rubenpa1998 <rubenpa1998@gmail.com>
2023-07-31 13:54:10 +0530

    Create LambdaExamples.java

---
+++ LambdaExamples.java
@@ -0,0 +1,46 @@
+
+
+interface Shape{
+    void draw();
+}
+
+/* Code is in polymorfic form down here 
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle Class : Draw() method");
+        
+    }
+}
+class Square implements Shape{
+     @Override
+     public void draw() {
+         System.out.println("Square Class : Draw() method");
+         
+     }
+}
+class Circle implements Shape{
+     @Override
+     public void draw() {
+         System.out.println("Circle Class : Draw() method");
+         
+     }
+} */
+
+public class LambdaExamples {
+
+   public static void main(String[] args) {
+    /* Lamdba Expessions Start */
+    Shape rectangle = () -> System.out.println("Rectangle Class : Draw() method");
+    rectangle.draw();
+
+    Shape square = () -> System.out.println("Sqaure Class : draw() method");
+    square.draw();
+
+    Shape circle = () -> System.out.println("Circle Class : draw() method");
+    circle.draw();
+     /* Lamdba Expessions End */
+   }
+    
+}
