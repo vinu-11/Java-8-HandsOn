f8f9fff4e479f6763385c116925a058cf07c496c
rubenpa1998 <rubenpa1998@gmail.com>
2023-07-31 17:49:58 +0530

    Created SupplierDemo Java file

---
+++ functional/SupplierDemo.java
@@ -0,0 +1,26 @@
+package functional;
+
+/* Generic function for the Interface Supplier <T> uses Current data */
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+class SupplierImpl implements Supplier<LocalDateTime> {
+
+    @Override
+    public LocalDateTime get() {
+        return LocalDateTime.now();
+    }
+
+}
+
+public class SupplierDemo {
+    public static void main(String[] args) {
+        // Supplier<LocalDateTime> supplier = new SupplierImpl();
+        // System.out.println(supplier.get());
+
+        Supplier<LocalDateTime> supplier = () -> LocalDateTime.now();
+        System.out.println(supplier.get());
+
+    }
+}
