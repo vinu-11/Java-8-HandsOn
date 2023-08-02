237f0f7201f0e5c873277f98546b10d03af6ff15
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-02 18:53:42 +0530

    Update : orELse

--- lambda/optional/OptionalDemo.java
+++ lambda/optional/OptionalDemo.java
@@ -6,19 +6,21 @@ public class OptionalDemo {
     public static void main(String[] args) {
         String emailId = null;
 
-        Optional<Object> emptyOptional = Optional.empty();
-        System.out.println(emptyOptional);
-
-        Optional<String> emailIdOptional = Optional.of(emailId);
-        System.out.println(emailIdOptional.get());
+//        Optional<Object> emptyOptional = Optional.empty();
+//        System.out.println(emptyOptional);
+//
+//        Optional<String> emailIdOptional = Optional.of(emailId);
+//        System.out.println(emailIdOptional.get());
 
         Optional<String> stringOptional = Optional.ofNullable(emailId);
+        String defaultVariable = stringOptional.orElse("default@gmail.com");  //orElse Method
+        System.out.println(defaultVariable);
 
-        if(stringOptional.isPresent()){
-            System.out.println(stringOptional.get());
-        }else{
-            System.out.println("No value Present");
-        }
+//        if(stringOptional.isPresent()){                       //isPresent() Method
+//            System.out.println(stringOptional.get());
+//        }else{
+//            System.out.println("No value Present");
+//        }
 
         //System.out.println(stringOptional.get());
     }
