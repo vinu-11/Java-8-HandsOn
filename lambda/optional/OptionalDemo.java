9a5e6f997f771b1c9951b13b6cb5a53e8a1981e8
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-02 19:18:18 +0530

    Update : isPresent() Method

--- lambda/optional/OptionalDemo.java
+++ lambda/optional/OptionalDemo.java
@@ -6,23 +6,35 @@ public class OptionalDemo {
     public static void main(String[] args) {
         String emailId = null;
 
+          //empty method
 //        Optional<Object> emptyOptional = Optional.empty();
 //        System.out.println(emptyOptional);
 //
+          //of method
 //        Optional<String> emailIdOptional = Optional.of(emailId);
 //        System.out.println(emailIdOptional.get());
 
+        //ofNullable
         Optional<String> stringOptional = Optional.ofNullable(emailId);
         String defaultVariable = stringOptional.orElse("default@gmail.com");  //orElse Method
         System.out.println(defaultVariable);
 
+        //orElseGet
         String defaultOptional = stringOptional.orElseGet(() -> "default@gmail.com");  //orElseGet Method Uses Supplier Interace
         System.out.println(defaultOptional);
 
-        String emailNotExist = stringOptional.orElseThrow(() -> new IllegalArgumentException("Email not exist"));
-        System.out.println(emailNotExist);
+        //orElseThrow
+//        String emailNotExist = stringOptional.orElseThrow(() -> new IllegalArgumentException("Email not exist"));
+//        System.out.println(emailNotExist);
 
+        //ifPresent Method
 
+        Optional<String> gender = Optional.of("Male");
+        Optional<String> emptyOptional = Optional.empty();
+
+        gender.ifPresent((s) -> System.out.println("Value is present"));
+        emptyOptional.ifPresent((s) -> System.out.println("Value is not present")); //Returns nothing
+        
 //        if(stringOptional.isPresent()){                       //isPresent() Method
 //            System.out.println(stringOptional.get());
 //        }else{
