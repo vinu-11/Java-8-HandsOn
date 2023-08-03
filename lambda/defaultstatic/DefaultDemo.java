08d20ce961f123a665373335642af499bb32ff46
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-03 12:28:11 +0530

    Updated : Added Default Method Call

--- lambda/defaultstatic/DefaultDemo.java
+++ lambda/defaultstatic/DefaultDemo.java
@@ -12,5 +12,33 @@ interface Vehicle{
         return "Turning Vehicle Alarm Off";
     }
 }
+
+class Car implements Vehicle{
+
+    @Override
+    public String getBrand() {
+        return "BMW";
+    }
+
+    @Override
+    public String speedUp() {
+        return "Car is Speeding Up";
+    }
+
+    @Override
+    public String slowDown() {
+        return "Car is slowing down";
+    }
+}
 public class DefaultDemo {
+    public static void main(String[] args) {
+        Car vehicle = new Car();
+        System.out.println(vehicle.getBrand());
+        System.out.println(vehicle.speedUp());
+        System.out.println(vehicle.slowDown());
+
+        //Default Method Call
+        System.out.println(vehicle.turnAlarmOff());
+        System.out.println(vehicle.turnAlarmOn());
+    }
 }
